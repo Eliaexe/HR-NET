428295e62561e4df43ff951fb43424c8cdc93468
Eliaexe <eliasidori@outlook.it>
2023-07-28 14:17:34 +0200

    Now the table is a npm library

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,53 @@
 import React, { useEffect, useState } from "react";
+import { useTable } from "react-table";
 
 export default function Table(props) {
-  const data = props.data;
-  const [headerData, setHeaderData] = useState([]);
+    const data = props.data;
 
-  useEffect(() => {
-    // Genera l'array delle chiavi per l'intestazione
-    const headerKeys = data.length > 0 ? Object.keys(data[0]) : [];
-    setHeaderData(headerKeys);
-  }, [data]);
+    const [headerData, setHeaderData] = useState([]);
+    function format(str) {
+      const words = str.split('_');    
+      const formattedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));    
+      const formattedStr = formattedWords.join(' ');    
+      return formattedStr;
+    }
+    useEffect(() => {
+      const headerKeys = data.length > 0 ? Object.keys(data[0]) : [];
+      const formattedKey = headerKeys.map((key) => format(key))
+      const tableHeader = formattedKey.map((head) => ({ Header: head, accessor: head }));
+      setHeaderData(tableHeader);
+    }, [data]);
+
+  const columns = headerData
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {/* Genera dinamicamente le celle dell'intestazione */}
-          {headerData.map((key) => (
-            <th key={key}>{key}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {/* Genera dinamicamente le righe del corpo della tabella */}
-        {data.map((rowData, index) => (
-          <tr key={index}>
-            {headerData.map((key) => (
-              <td key={key}>{rowData[key]}</td>
+    <table {...getTableProps()}>
+      <thead> 
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
             ))}
           </tr>
         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
-}
+};
+
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,18 +4,15 @@ import { states } from "../utils/states";
 import { department } from "../utils/department";
 
 export function Create() {
-
     const statesName = []
     
     states.forEach(e => {
       statesName.push(e.name)
     });
 
-
     return(
         <div className="mainCreate">
             <Header />
-            
             <Modal 
                 modalName={'Create Employee'} 
                 input={{
